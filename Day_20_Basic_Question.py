3f18cd059e7d0898b6ef9d13fd8b683ee04d475e
ShahZakii <shahhzaki@gmail.com>
2026-02-12 23:27:35 +0530

    Added basic question

--- Day_20_Basic_Question.py
+++ Day_20_Basic_Question.py
@@ -41,3 +41,17 @@ for i in range(1,n+1):
     type = str(i)
     newstr += type
 print(newstr)
+
+
+# Question 3:
+# Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. Then compute and print 
+# the result of hash(t).
+
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+
+n = int(input())
+
+integer_list = map(int,input().split())
+integer_tuple = tuple(integer_list)
+
+print(hash(integer_tuple))
