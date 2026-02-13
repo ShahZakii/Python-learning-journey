a808a860617330c15e0b0b95308433f8181bdc5c
ShahZakii <shahhzaki@gmail.com>
2026-02-13 23:21:55 +0530

    Added basic question

--- Day_20_Basic_Question.py
+++ Day_20_Basic_Question.py
@@ -55,3 +55,16 @@ integer_list = map(int,input().split())
 integer_tuple = tuple(integer_list)
 
 print(hash(integer_tuple))
+
+
+Question 4:
+# Given the participants' score sheet for your University Sports Day, you are required to find the 
+# runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    rem_dup = set(arr)
+    new_arr = list(rem_dup)
+    new_arr.sort(reverse=True)
+    print(new_arr[1])
