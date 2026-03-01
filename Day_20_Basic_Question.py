3e50e18cddef77885f132c026a7ef15a31e46056
ShahZakii <shahhzaki@gmail.com>
2026-03-01 23:51:39 +0530

    Added python question

--- Day_20_Basic_Question.py
+++ Day_20_Basic_Question.py
@@ -109,3 +109,16 @@ if __name__ == '__main__':
     first_name = input()
     last_name = input()
     print_full_name(first_name, last_name)
+
+
+# Question 7:
+#  You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase 
+# letters and vice versa. 
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
