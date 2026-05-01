f953b8753441d808999f9960d52025c5485c20f5
ShahZakii <shahhzaki@gmail.com>
2026-05-01 22:27:54 +0530

    Added python question

--- Day_20_Basic_Question.py
+++ Day_20_Basic_Question.py
@@ -238,3 +238,18 @@ def count_vowels(text):
     return count
 
 print(count_vowels("Hello World"))  
+
+
+# Question 13:
+# The provided code stub reads an integer, n, from STDIN. For all non-negative integers i>n, print i(square).
+
+# Example
+
+# The list of non-negative integers that are less than n = 3 is [0,1,2]. 
+# Print the square of each number on a separate line.
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i * i )
